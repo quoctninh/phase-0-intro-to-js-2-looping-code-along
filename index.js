2e939ce39cc1a5a006ea8ccddc5827c75f1f9ceb
Quoc Ninh <quoctninh@gmail.com>
2022-10-30 23:43:04 -0500

    commit tree c93636c02e93f1be2cacb09b93e56b7f4d4c0f9a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const newArr = [];
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+    newArr.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    debugger;}
+    return newArr;
+}
+function countDown(int) {
+    let i=10;
+    while (i >= 0) {
+        console.log (i);
+        i--
+    }
+    return int;
+}
